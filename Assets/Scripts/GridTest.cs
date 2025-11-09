7bef9e58e283a1abb5b9d1a321babe138e0a11ef
YunhoLee <yl4643@nyu.edu>
2025-11-09 10:18:02 +0900

    Update : Test Code

---
+++ Assets/Scripts/GridTest.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class GridTest : MonoBehaviour
+{
+    public GameObject cube, blockPrefab;
+    public Grid grid;
+    public GridTestInput gridInput;
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField]
+    private Camera sceneCamera;
+
+    private Vector3 lastPosition;
+
+    [SerializeField]
+    private LayerMask placementLayermask;
+
+    public Vector3 GetSelectedMapPosition()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = sceneCamera.nearClipPlane;
+        Ray ray = sceneCamera.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100, placementLayermask))
+        {
+            lastPosition = hit.point;
+        }
+        return lastPosition;
+    }
+}
